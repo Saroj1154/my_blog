e7637170f1d20c1429e34e6b82865a9d563087a8
saroj <pandeysarojofficial@gmail.com>
2021-01-28 14:28:18 +0545

    Spacing

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("This is a <strong> bold </strong> song and this is done by Rupesh Dahal")# Create your views here.
+    return HttpResponse("T           his is a <strong> bold </strong> song and this is done by Rupesh Dahal")# Create your views here.
